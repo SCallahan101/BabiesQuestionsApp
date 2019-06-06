65355955e65808587e370dc85359680fd6d70f6d
SCallahan101 <Shannonc619@gmail.com>
2019-06-06 12:37:43 -0600

    testing with error message

--- FrontMain/index.js
+++ FrontMain/index.js
@@ -133,27 +133,26 @@ function loginEntry(user, pw){
       // const confirmedJWT = new localStrategy(data.user, data.password, callback);
     },
     error: function (jqXHR, exception) {
-        var msg = '';
-        if (jqXHR.status === 0) {
-            msg = 'Not connect.\n Verify Network.';
-        } else if (jqXHR.status == 404) {
-            msg = 'Requested page not found. [404]';
-        } else if (jqXHR.status == 500) {
-            msg = 'Internal Server Error [500].';
-        } else if (exception === 'parsererror') {
-            msg = 'Requested JSON parse failed.';
-        } else if (exception === 'timeout') {
-            msg = 'Time out error.';
-        } else if (exception === 'abort') {
-            msg = 'Ajax request aborted.';
-        } else if (exception === 'parsererror') {
-            console.log(JSON.stringify(exception));
-            msg = 'Requested JSON parse failed.';
-        } else {
-            msg = 'Uncaught Error.\n' + jqXHR.responseText;
-        }
-      console.log(msg);
-    }
+       console.log("sanity check, log in error callback");
+       var msg = '';
+       if (jqXHR.status === 0) {
+           msg = 'Not connect.\n Verify Network.';
+       } else if (jqXHR.status == 404) {
+           msg = 'Requested page not found. [404]';
+       } else if (jqXHR.status == 500) {
+           msg = 'Internal Server Error [500].';
+       } else if (exception === 'parsererror') {
+           console.log(JSON.stringify(exception));
+           msg = 'Requested JSON parse failed.';
+       } else if (exception === 'timeout') {
+           msg = 'Time out error.';
+       } else if (exception === 'abort') {
+           msg = 'Ajax request aborted.';
+       } else {
+           msg = 'Uncaught Error.\n' + jqXHR.responseText;
+       }
+     console.log(msg);
+   }
   };
   console.log(loginData);
   $.ajax(loginData);
