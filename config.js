9a2367ed7ff0fd855f53c35219a697881769ca49
Shannon Callahan <Shannonc619@gmail.com>
2021-01-15 20:19:26 -0700

    update config with new mongodb atlas

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 "use strict";
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://boardCreator619:board123@ds111025.mlab.com:11025/questionsboard';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb+srv://boardCreator619:board123@questionsboard.lchux.mongodb.net/questionsboard?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test_database_url';
 exports.PORT = process.env.PORT || 4747;
 
 exports.JWT_SECRET = process.env.JWT_SECRET || 'jwtSecretPw';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '3d';
+
+
+// mongodb://boardCreator619:board123@ds111025.mlab.com:11025/questionsboard
